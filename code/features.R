744f76582b8e0e60e4ffe577683024adf2056b3a
leilacodes <electriclam@gmail.com>
2018-08-27 13:29:52 -0700

    add code to get cumulative wins and losses for matches

--- code/features.R
+++ code/features.R
@@ -5,79 +5,20 @@ source('code/data_import.R')
 
 aggdata <- entry_info %>% filter(year(tourney_date) > 1987)
 
-splitdata <- split(aggdata, aggdata$sourcefile)
-
-getlosses <- function(x) {
-  print(x[1,]$sourcefile)
-  
-  players <- x %>% select(winner_id, loser_id) %>% unique()
-  
-  loss_subset <- career_losses_vs %>%
-    select(loser_id, winner_id, tourney_date, career_losses) %>% 
-    inner_join(players) %>% 
-    filter(tourney_date < max(x$tourney_date))
-    
-  output <- x %>% 
-    fuzzy_left_join(loss_subset,
-                    by = c("winner_id" = "loser_id",
-                           "loser_id" = "winner_id", 
-                           "tourney_date" = "tourney_date"), 
-                    match_fun = c(`==`, `==`, `>`)) %>%
-    replace_na(replace = list(career_losses = 0)) %>% 
-    group_by(match_id) %>% 
-    filter(career_losses == max(career_losses)) %>% 
-    select(-ends_with('.y'))
-  
-  return(output)
-}
-
-tic()
-withlosses <- map_dfr(splitdata , getlosses)
-toc()
-
-# Get wins
-getwins <- function(x) {
-  print(x[1,]$sourcefile)
-  
-  players <- x %>% select(winner_id, loser_id) %>% unique()
-  
-  win_subset <- career_wins_vs %>%
-    select(loser_id, winner_id, tourney_date, career_wins) %>% 
-    inner_join(players) %>% 
-    filter(tourney_date < max(x$tourney_date))
-  
-  output <- x %>% 
-    fuzzy_left_join(win_subset,
-                    by = c("winner_id" = "winner_id",
-                           "loser_id" = "loser_id", 
-                           "tourney_date" = "tourney_date"), 
-                    match_fun = c(`==`, `==`, `>`)) %>%
-    replace_na(replace = list(career_wins = 0)) %>% 
-    group_by(match_id) %>% 
-    filter(career_wins == max(career_wins)) %>% 
-    select(-ends_with('.y'))
-  
-  return(output)
-}
-
-tic()
-withwins <- map_dfr(splitdata, getwins)
-toc()
-
-# Whew save the data!
-saveRDS(withlosses, file = "data/withlosses.RDS")
-saveRDS(withwins, filie = "data/withwins.RDS")
-
-# Check that it worked
-
 #QA
-graph_freqs(entry_info)
+graph_freqs(aggdata)
+
+freq(aggdata$surface)
+
+aggdata %>% filter(surface == "") %>% group_by(sourcefile) %>% View()
+
+firstlook(aggdata)
 
 # Designate each match as a win or loss
 set.seed <- 11271989
 
-outcomedata <- entry_info %>% 
-  bind_cols(., win = rbernoulli(nrow(entry_info)))
+outcomedata <- aggdata %>% 
+  bind_cols(., win = rbernoulli(nrow(aggdata)))
 
 splitdata <- split(outcomedata, outcomedata$win)
 
@@ -109,71 +50,121 @@ prep_dset <- function(dset, prefix) {
   return(dset)
 }
 
-# Define features (Including pulling in match records)
-# The first join 
-prepped_dset <- map_dfr(splitdata, prep_dset) %>%
+# Apply randomization and recombine dataset
+# Add a few features
+modeldset <- map_dfr(splitdata, prep_dset) %>%
   mutate(rank_point_diff = player_rank_points - opponent_rank_points,
          age_diff = player_age - opponent_age) 
 
-# Career losses
-tic()
-agg_losses <- map_dfr(split_dset, function(x) {
-  x %>% ste, match_id) %>% 
-    fuzzy_left_join(career_losses_vs %>% select(loser_id, winner_id, tourney_date, career_losses, match_id),  
-                                                            by = c("player_id" = "loser_id",
-                                                                   "opponent_id" = "winner_id",
-                                                                   "tourney_date" = "tourney_date"),
-                    match_fun = list(`==`, `==`, `>`)) %>% 
-    group_by(match_id.x) %>% 
-    replace_na(replace = list(career_losses = 0)) %>% 
-    filter(career_losses == max(career_losses)) %>% 
-    select(-ends_with(".y"), -loser_id, -winner_id)
-})
-toc()
+splitdata <- split(modeldset, modeldset$sourcefile)
 
-tic()
-agg_wins <- map_dfr(split_dset, function(x) {
-  x %>% select(player_id, opponent_id, tourney_date, match_id) %>% 
-    fuzzy_left_join(career_wins_vs %>% select(loser_id, winner_id, tourney_date, career_wins, match_id),  
-                    by = c("player_id" = "loser_id",
-                           "opponent_id" = "winner_id",
-                           "tourney_date" = "tourney_date"),
-                    match_fun = list(`==`, `==`, `>`)) %>% 
-    group_by(match_id.x) %>% 
-    replace_na(replace = list(career_wins = 0)) %>% 
-    filter(career_wins == max(career_wins)) %>% 
-    select(-ends_with(".y"), -loser_id, -winner_id)
-})
-toc()
-
-  # fuzzy_left_join(x = .,
-  #                 y = career_losses_vs,
-                  # by = c("player_id" = "loser_id",
-                  #        "opponent_id" = "winner_id",
-                  #        "tourney_date" = "tourney_date"),
-                  # match_fun = list(`==`, `==`, `>`)) %>%
-  # fuzzy_anti_join(career_losses_vs %>% select(loser_id, winner_id, tourney_date),
-  #                 by = c("loser_id" = "loser_id",
-  #                        "winner_id" = "winner_id",
-  #                        "tourney_date" = "tourney_date"),
-  #                 match_fun = list(`==`, `==`, `>`))
-
+getlosses <- function(x) {
+  print(x[1,]$sourcefile)
   
+  players <- x %>% select(player_id, opponent_id) %>% unique()
+  
+  loss_subset <- career_losses_vs %>%
+    select(loser_id, winner_id, tourney_date, career_losses) %>% 
+    inner_join(players, by = c("loser_id" = "player_id",
+                               "winner_id" = "opponent_id")) %>% 
+    filter(tourney_date < max(x$tourney_date))
+  
+  output <- x %>% 
+    fuzzy_left_join(loss_subset,
+                    by = c("player_id" = "loser_id",
+                           "opponent_id" = "winner_id", 
+                           "tourney_date" = "tourney_date"), 
+                    match_fun = c(`==`, `==`, `>`)) %>%
+    replace_na(replace = list(career_losses = 0)) %>% 
+    group_by(match_id) %>% 
+    filter(career_losses == max(career_losses)) %>% 
+    select(-ends_with('.y'))
+  
+  return(output)
+}
 
-firstlook(prepped_dset)
+tic()
+withlosses <- map_dfr(splitdata , getlosses) %>% rename(tourney_date = tourney_date.x)
+toc()
+
+# Get wins
+getwins <- function(x) {
+  
+  print(x[1,]$sourcefile)
+  
+  players <- x %>% select(player_id, opponent_id) %>% unique()
+  
+  win_subset <- career_wins_vs %>%
+    select(loser_id, winner_id, tourney_date, career_wins) %>% 
+    inner_join(players, by = c("winner_id" = "player_id",
+                               "loser_id" = "opponent_id")) %>% 
+    filter(tourney_date < max(x$tourney_date))
+  
+  output <- x %>% 
+    fuzzy_left_join(win_subset,
+                    by = c("player_id" = "winner_id",
+                           "opponent_id" = "loser_id", 
+                           "tourney_date" = "tourney_date"), 
+                    match_fun = c(`==`, `==`, `>`)) %>%
+    replace_na(replace = list(career_wins = 0)) %>% 
+    group_by(match_id) %>% 
+    filter(career_wins == max(career_wins)) %>% 
+    select(-ends_with('.y'))
+  
+  return(output)
+}
+
+tic()
+withwins <- map_dfr(splitdata, getwins) %>% rename(tourney_date = tourney_date.x)
+toc()
+
+# Whew save the data!
+saveRDS(withlosses, file = "data/withlosses.RDS")
+saveRDS(withwins, file = "data/withwins.RDS")
+
+# Check that it worked
+match_list %>% filter((winner_id == "202469" & loser_id == "201347") |
+                        (winner_id == "201347" & loser_id == "202469")) %>%
+  select(winner_id, loser_id, tourney_date) %>% arrange(tourney_date)
+
+withwins %>% filter(player_id == "201347", opponent_id == "202469") %>% 
+  select(player_name, opponent_name, tourney_date.x, career_wins)
+
+withlosses %>% filter(player_id == "201347", opponent_id == "202469")%>%
+  select(player_name, opponent_name, tourney_date.x, career_losses)
 
 # Add features
+# Fed Cup website says youngest player was 12
+prepped_dset <- modeldset %>% 
+  left_join(withwins) %>% 
+  left_join(withlosses) %>% 
+  filter(player_age >= 12, opponent_age >= 12) 
+  
+# Ranking points changed in 2016 not to count the Olympics
+# Fill in ranking points that are missing?
+# Remove temp dsets
+firstlook(prepped_dset)
+varinfo(prepped_dset) %>% arrange(desc(ndistinct))
+
+# Cannot find Laura Hernandez online -- delete? Tweeted Jeff Sackman
+# Delete age under 12 since Fed Cup website says youngest player was 12
+prepped_dset %>% filter(player_age < 13 | opponent_age < 13)
+prepped_dset %>% filter(player_name == "Denise")
+
 
 # Commented out if no relationship visible
 plotlift <- function(x) {
   plot_ntile_lift(prepped_dset, quo(win), x)
 }
 
+ggplot(data = prepped_dset, aes(x = player_rank_points)) + geom_density()
+
 plotlift(quo(player_rank_points))
 plotlift(quo(opponent_rank_points))
 plotlift(quo(rank_point_diff))
-plotlift(quo(player_age))
-plotlift(quo(opponent_age))
+plotlift(quo(age_diff))
+plotlift(quo(career_wins))
+plotlift(quo(career_losses))
 
 # plot_ntile_lift(prepped_dset, quo(age_diff))
 
