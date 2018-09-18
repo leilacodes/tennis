213b76192a8f5f6f26a047a6ee8a7e027eacb3e3
Alex Beltyukov <beltyukov.1@gmail.com>
2018-09-18 11:38:27 -0700

    uni instead of my

--- dashboard/app.R
+++ dashboard/app.R
@@ -1,10 +1,8 @@
 library(shiny)
 library(shinydashboard)
-source('../functions/shiny_functions.R')
-
 
 # Load Data ---------------------------------------------------------------
-
+source('../functions/shiny_functions.R')
 rawdata <- readRDS('../data/model_dset.RDS')
 
 # Uni ---------------------------------------------------------------------
@@ -72,7 +70,38 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "unitab",
-            box(title = "Select Variable")
+            fluidPage(
+              
+              # Application title
+              titlePanel("Univariate Information"),
+              
+              # Sidebar with a slider input for number of bins 
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput(inputId = "univar",
+                              label = "Select columns:", 
+                              choices = unicolnames, 
+                              selected = unicolnames[1], 
+                              multiple = FALSE,
+                              selectize = TRUE,
+                              width = NULL, 
+                              size = NULL),
+                  checkboxGroupInput(inputId = "mytransform",
+                                     label = "Transform Variable?", 
+                                     choices = transformations,
+                                     selected = NULL,
+                                     inline = TRUE, 
+                                     width = NULL)
+                ),
+                
+                # Show a plot of the generated distribution
+                mainPanel(
+                  plotOutput(outputId = "uniplot", 
+                             width = "100%",
+                             height = "400px")
+                )
+              )
+            )
             # h1("Explore Univariate Distributions")
     ),
     tabItem(tabName = "bitab",
@@ -87,14 +116,46 @@ body <- dashboardBody(
 )
 
 ui <- dashboardPage(dashboardHeader(title = "WTA Predictive Model Data"),
-              sidebar,
-              body)
+                    sidebar,
+                    body)
 
 
 # Define Server -----------------------------------------------------------
 
 
 server <- function(input, output) {
+  output$uniplot <- renderPlot({
+    
+    if(length(input$mytransform) > 1) {
+      stop("Max 1 transformation")
+    }
+    
+    if(is.null(input$mytransform)) {
+      
+      plotdata <- unidata
+      
+    } else {
+      
+      plotdata <- unidata %>% 
+        mutate_at(.vars = input$univar, 
+                  .funs = eval(parse(text = input$mytransform)))
+      
+    }
+    
+    if(length(unique(unidata[,input$univar])) > 20) {
+      
+      ggplot(data = plotdata) +
+        geom_density(aes_string(x = input$univar)) + 
+        labs(title = glue("Density of Values for {input$univar}"))
+      
+    } else {
+      
+      ggplot(data = plotdata) +
+        geom_bar(aes_string(x = input$univar)) + 
+        labs(title = glue("Frequency of Values for {input$univar}"))
+      
+    } 
+  })
 }
 
 shinyApp(ui, server)
