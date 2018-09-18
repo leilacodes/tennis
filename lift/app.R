837c2c9ecd1905d96214017fc3e7dbf51c3b66a4
Alex Beltyukov <beltyukov.1@gmail.com>
2018-09-18 11:38:54 -0700

    try to account for NAs generated by transformations

--- lift/app.R
+++ lift/app.R
@@ -1,3 +1,5 @@
+# Shiny dashboard
+# TODO: Why is the content not showing?
 source('../functions/shiny_functions.R')
 
 liftdata <- readRDS('../data/model_dset.RDS') %>% 
@@ -22,21 +24,22 @@ ui <- fluidPage(
   # Sidebar with a dropdown menu for variable
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "x",
-                  label = "Select X variable:", 
-                  choices = liftcolnames, 
-                  selected = liftcolnames[1], 
-                  multiple = FALSE,
-                  selectize = TRUE,
-                  width = NULL, 
-                  size = NULL)
+      radioButtons(inputId = "liftvar", 
+                   label = "Select X variable:",
+                   choices = liftcolnames, 
+                   selected = liftcolnames[1], 
+                   inline = FALSE),
+      checkboxGroupInput(inputId = "lifttransform",
+                         label = "Transform Variable?", 
+                         choices = transformations,
+                         selected = NULL,
+                         inline = TRUE, 
+                         width = NULL)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput(outputId = "varplot", 
-                 width = "100%",
-                 height = "400px")
+      plotOutput(outputId = "varplot")
     )
   )
 )
@@ -46,17 +49,41 @@ server <- function(input, output) {
   
   output$varplot <- renderPlot({
     
-    if(!is.numeric(liftdata[, input$x]) & length(unique(liftdata[, input$x])) > 10) {
+    if(!is.numeric(liftdata[, input$liftvar]) & 
+       length(unique(liftdata[, input$liftvar])) > 0) {
       stop("Numeric or categorical only")
     }
     
-    if(length(unique(liftdata[, input$x])) > 10) {
+    
+    if(length(input$lifttransform) > 1) {
+      stop("Max 1 transformation")
+    }
+    
+    if(is.null(input$lifttransform)) {
       
-      plot_lift_range(dset = liftdata, xvar = input$x)
+      plotdata <- liftdata
       
     } else {
       
-      plot_lift(dset = liftdata, xvar = input$x)
+      plotdata <- liftdata %>% 
+        mutate_at(.vars = input$liftvar,
+                  .funs = function(x) {
+                    x0 <- x
+                    x0[which(x==0)] <- .000000001
+                    return(x0)
+                  }) %>% 
+        mutate_at(.vars = input$liftvar, 
+                  .funs = eval(parse(text = input$lifttransform)))
+      
+    }
+    
+    if(length(unique(liftdata[, input$liftvar])) > 300) {
+      
+      plot_lift_range(dset = plotdata, xvar = input$liftvar)
+      
+    } else {
+      
+      plot_lift(dset = plotdata, xvar = input$liftvar)
       
     }
     
